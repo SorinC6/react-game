1cfd9d1f997a76108a1451d5c558c8b6e776ba69
Sorin Chis <kish.sorin@yahoo.com>
2019-11-05 20:22:40 +0200

    Updates

---
+++ precache-manifest.5b4ac44d148bd6525fccee095f02f688.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c043c955a4147436c0816cdec30dcc9c",
+    "url": "/Code-Challenge-Aurity/index.html"
+  },
+  {
+    "revision": "fb2b987f022bde984176",
+    "url": "/Code-Challenge-Aurity/static/css/2.aa7fcd32.chunk.css"
+  },
+  {
+    "revision": "cdadbf5d1ffa9dc1f00b",
+    "url": "/Code-Challenge-Aurity/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "fb2b987f022bde984176",
+    "url": "/Code-Challenge-Aurity/static/js/2.4b448296.chunk.js"
+  },
+  {
+    "revision": "cdadbf5d1ffa9dc1f00b",
+    "url": "/Code-Challenge-Aurity/static/js/main.56cdd026.chunk.js"
+  },
+  {
+    "revision": "f66c2e1ce0a0e1f4a9a2",
+    "url": "/Code-Challenge-Aurity/static/js/runtime-main.6c2c3e77.js"
+  }
+]);
